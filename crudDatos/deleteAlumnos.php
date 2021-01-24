5ff9fb2fa2f277402a806623cdb24642fcfbc49d
SebastianAvila <jesusaviala@gmail.com>
2021-01-23 23:46:04 -0600

    Se agrego la consulta para borrar la cuenta becario

--- crudDatos/deleteAlumnos.php
+++ crudDatos/deleteAlumnos.php
@@ -8,10 +8,16 @@ $id_UnicoAlum = $_GET['id_UnicoAlum'];
 $queryDelete="DELETE FROM ALUMNOS WHERE id_UnicoAlum='$id_UnicoAlum'";
 $resultado =mysqli_query ($coneccion, $queryDelete);
 
- if($resultado){
+$queryDeleteCuenta ="DELETE FROM becariocuenta WHERE id_UnicoAlum='$id_UnicoAlum'";
+$resultadoDelete =mysqli_query ($coneccion, $queryDeleteCuenta);
+ if($resultado and $resultadoDelete){
  header("Location:../crudDatos/crudAlumnos.php");
  }else{
     ?> <h3>A ocurrido un error</h3> <?php
  }
+
+
+
+
 }
 ?>
