963895cf024f42b505ae70bb512e89c6b064a1e2
SebastianAvila <jesusavila@gmail.com>
2022-05-17 15:30:03 -0500

    cambios login admin/user

--- login/loginBecario.php
+++ login/loginBecario.php
@@ -1,30 +1,59 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../css/loginAdmin.css">
-    <title>Sistema Becario</title>
-</head>
-<body>
-    <div class="container p-4 card card-body divPrincipal"> 
-    <form action="autentificacion_becario.php" action="../sistemabecario/crearbecario.php" method="POST">
-        <h1 class="text-center">Becarios</h1><br>
-        <img src="../Imagenes/UADY_logo.png" class="img-fluid imgAdmin"/>
-        <h3 class="text-center">Usuario:</h3>
-        <input type="text" class="form-control " name="be_usuario" placeholder="&#128272; Usuario"  ><br>
-        <h3 class="text-center">Contraseña:</h3>
-        <input type="password" class="form-control" name="be_contra" placeholder=" &#128273; Contraseña"       &#128273;><br>
-        <br> 
-        <input type="submit"  class="btn btn-success btn-block" value="Iniciar Sesion"> <br>
 
-        <a href="../index.php"> Regresar al inicio</a>
+<?php include('../layout/header.php'); ?>
+
+<link rel="stylesheet" href="../css/loginAdmin.css">
+<div class="container p-4 card card-body divPrincipal">
+
+    <div class="container">
+
+        <div id="loginbox" class="mainbox col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3">
+
+            <div class="row">
+                <img src="../imagenes/UADY_logo.png" alt="logoUady" style="width: 570px; height: 275px;">
+            </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <div class="panel-title text-center">Login Becario</div>
+                </div>
+
+                <div class="panel-body">
+
+                    <form  action="autentificacion_becario.php" name="form" id="form" class="form-horizontal" enctype="multipart/form-data" method="POST">
+                        <?php if (isset($_SESSION['message'])) {   ?>
+                            <div class="alert alert-<?= $_SESSION['message_type'] ?> alert-dismissible fade show" role="alert">
+                                <?= $_SESSION['message']  ?>
+                               <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                            </div>
+                        <?php session_destroy();
+                        } ?>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                            <input id="ad_usuario" type="text" class="form-control" name="be_usuario" value="" placeholder="User">
+                        </div>
+
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                            <input id="ad_contra" type="password" class="form-control" name="be_contra" placeholder="Password">
+                        </div>
+
+                        <div class="form-group">
+                            <!-- Button -->
+                            <div class="col-sm-12 controls">
+                                <button type="submit" href="#" class="btn btn-primary pull-right"><i class="glyphicon glyphicon-log-in"></i> Log in</button>
+                                <a href="../index.php"><button href="#" class="btn btn-secondary pull-right">Regresar</button>
+                                </a>
+                            </div>
+                        </div>
+
+                    </form>
+
+                </div>
+            </div>
+        </div>
     </div>
-    </form>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
-    
-</body>
-</html>
+
+
+
+</div>
+<?php include('../layout/footer.php'); ?>
